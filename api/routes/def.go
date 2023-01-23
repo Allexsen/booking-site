88f7291bf446ba796913880d0fd98efae5dd3ec7
Alex <asvanidze12@gmail.com>
2023-01-23 20:46:41 +0400

    Bit of routes

--- api/routes/def.go
+++ api/routes/def.go
@@ -6,7 +6,15 @@ func Init() {
 	gin.ForceConsoleColor()
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Landing Page Hit")
+		c.String(200, "Landing page hit")
+	})
+
+	r.GET("/booking", func(c *gin.Context) {
+		c.String(200, "Booking page hit")
+	})
+
+	r.GET("/reviews", func(c *gin.Context) {
+		c.String(200, "Reviews page hit")
 	})
 
 	r.Run(":5000")
