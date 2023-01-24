46b977b7193e0cbcd72856553811fb0fbc51d5cd
Alex <asvanidze12@gmail.com>
2023-01-24 17:46:56 +0400

    Routing Setup

---
+++ api/routes/bookings.go
@@ -0,0 +1,17 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func initBookings(r *gin.Engine) {
+	r.GET("/bookings", func(c *gin.Context) {
+		c.String(200, "Booking page hit")
+	})
+
+	r.POST("/bookings", func(c *gin.Context) {
+		c.String(200, "New Booking hit")
+	})
+
+	r.GET("/bookings/refund/:token", func(c *gin.Context) {
+		c.String(200, "Refund hit")
+	})
+}
--- api/routes/def.go
+++ api/routes/def.go
@@ -5,16 +5,12 @@ import "github.com/gin-gonic/gin"
 func Init() {
 	gin.ForceConsoleColor()
 	r := gin.Default()
+
+	initReviews(r)
+	initBookings(r)
+
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Landing page hit")
-	})
-
-	r.GET("/booking", func(c *gin.Context) {
-		c.String(200, "Booking page hit")
-	})
-
-	r.GET("/reviews", func(c *gin.Context) {
-		c.String(200, "Reviews page hit")
 	})
 
 	r.Run(":5000")
