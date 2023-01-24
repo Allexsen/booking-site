01be586336f9ff2c770ba827af107f1fecdcc94f
Alex <asvanidze12@gmail.com>
2023-01-24 19:04:40 +0400

    Re - bookings

--- api/routes/bookings.go
+++ api/routes/bookings.go
@@ -8,7 +8,7 @@ func initBookings(r *gin.Engine) {
 	})
 
 	r.POST("/bookings", func(c *gin.Context) {
-		c.String(200, "New Booking hit")
+		c.String(200, "Booking Payment hit")
 	})
 
 	r.GET("/bookings/refund/:token", func(c *gin.Context) {
