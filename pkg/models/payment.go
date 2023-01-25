1c68877685765e7101de07105f514c48e5ad66c9
Alex <asvanidze12@gmail.com>
2023-01-25 15:14:22 +0400

    Payments - Methods

--- pkg/models/payment.go
+++ pkg/models/payment.go
@@ -1,13 +1,36 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	database "github.com/Allexsen/booking-site/db"
+)
 
 // Used to model payments
 type Payment struct {
-	ID        int
-	BookingID int
-	Method    string
-	Amount    int
-	DateTime  time.Time
-	Refunded  bool
+	ID              int
+	BookingID       int
+	Method          string
+	Amount          int
+	TransactionTime time.Time
+	Refunded        bool
+}
+
+func (p Payment) Store() error {
+	db := database.GetDB()
+	q := `INSERT INTO payments(booking_id, method, amount, transaction_time)
+		VALUES(?, ?, ?, ?)`
+
+	_, err := db.Exec(q, p.BookingID, p.Method, p.Amount, p.TransactionTime)
+	return err
+}
+
+func (p Payment) Refund() error {
+	db := database.GetDB()
+	q := `UPDATE payments
+		SET refunded=true
+		WHERE id=?`
+
+	_, err := db.Exec(q, p.ID)
+	return err
 }
