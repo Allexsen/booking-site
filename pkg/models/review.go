ceb9a48daed5b468d09763beb11bcb6069c3c353
Alex <asvanidze12@gmail.com>
2023-01-25 16:01:44 +0400

    Review - Methods

--- pkg/models/review.go
+++ pkg/models/review.go
@@ -1,5 +1,7 @@
 package models
 
+import database "github.com/Allexsen/booking-site/db"
+
 // Used to model reviews
 type Review struct {
 	ID        int
@@ -7,4 +9,13 @@ type Review struct {
 	Body      string
 	Author    string
 	BookingID int
+}
+
+func (r *Review) Store() error {
+	db := database.GetDB()
+	q := `INSERT INTO reviews(rating, body, author, booking_id)
+		VALUES(?, ?, ?, ?)`
+
+	_, err := db.Exec(q, r.Rating, r.Body, r.Author, r.BookingID)
+	return err
 }
