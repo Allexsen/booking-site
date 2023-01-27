85f97ab02c7c491733210e75c0ad12cc7f280772
Alex <asvanidze12@gmail.com>
2023-01-27 16:58:27 +0400

    Re - Review Route

--- api/routes/reviews.go
+++ api/routes/reviews.go
@@ -1,11 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Allexsen/booking-site/pkg/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func initReviews(r *gin.Engine) {
-	r.GET("/reviews", func(c *gin.Context) {
-		c.String(200, "Reviews page hit")
-	})
+	r.GET("/reviews", controllers.LoadReviews)
 
 	r.GET("/reviews/add/:token", func(c *gin.Context) {
 		c.String(200, "New Review GET hit on "+c.Param("token"))
