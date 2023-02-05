98c43322cd582837fd12ab5f314b711e0b8ddfc1
Alex <asvanidze12@gmail.com>
2023-02-05 15:56:35 +0400

    err update

--- api/routes/tmpl.go
+++ api/routes/tmpl.go
@@ -14,30 +14,31 @@ type Page struct {
 	Message    string
 }
 
-var (
-	desc map[int]string
-	msgs map[int]string
-)
+// var (
+// 	desc map[int]string
+// 	msgs map[int]string
+// )
 
-func init() {
-	desc[400] = "Bad Request!"
-	desc[403] = "Forbidden"
-	desc[404] = "Page not found!"
+// func init() {
+// 	desc = make(map[int]string)
+// 	desc[400] = "Bad Request!"
+// 	desc[403] = "Forbidden"
+// 	desc[404] = "Page not found!"
 
-	msgs[400] = "Invalid request - can't be processed"
-	msgs[403] = "Request not permitted"
-	msgs[404] = "We are sorry, the requested page can't be found."
-}
+// 	msgs = make(map[int]string)
+// 	msgs[400] = "Invalid request - can't be processed"
+// 	msgs[403] = "Request not permitted"
+// 	msgs[404] = "We are sorry, requested page couldn't be found."
+// }
 
 func initTemplates() {
 	r.NoRoute(func(c *gin.Context) {
 		tmpl := template.Must(template.ParseFiles(dirTMPL + "/err.html"))
-		stCode := c.Request.Response.StatusCode
 		page := Page{
-			Title:      "Error" + desc[stCode],
-			StatusCode: string(rune(stCode)),
-			Desc:       desc[stCode],
-			Message:    msgs[stCode],
+			Title:      "404 - Page not found",
+			StatusCode: "404",
+			Desc:       "Page not found",
+			Message:    "We are sorry, requested page couldn't be found.",
 		}
 
 		err := tmpl.Execute(c.Writer, page)
